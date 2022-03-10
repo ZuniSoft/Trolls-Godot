4af721520b4f9de6b8cb6df235c4a7bfbd4595ee
Keith Davis <keith.davis@zunisoft.com>
2022-03-09 16:24:14 -0800

    Added throwing anim to player for fireballs

--- Scripts/Hero.gd
+++ Scripts/Hero.gd
@@ -6,6 +6,7 @@ signal fireball_used
 var velocity = Vector2(0, 0)
 var is_hit = false
 var is_dying = false
+var is_throwing = false
 var in_ladder_area = false
 var has_fireballs = true
 var mystery_items = ["res://Scenes/Fireballs.tscn", "res://Scenes/Heart.tscn", "res://Scenes/Coin.tscn"]
@@ -33,20 +34,24 @@ func _physics_process(_delta):
 				velocity.x = -WALK_SPEED
 			else:
 				velocity.x = 0
-			$AnimatedSprite.play("jumping") 
-		elif Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_attack"):
+			$AnimatedSprite.play("jumping")
+		elif Input.is_action_just_pressed("ui_focus_next") and not in_ladder_area and has_fireballs:
+			is_throwing = true
+			$ThrowTimer.start()
+			$AnimatedSprite.play("throwing")
+		elif Input.is_action_pressed("ui_right") and not in_ladder_area and Input.is_action_pressed("ui_attack"):
 			$SoundSword.play()
 			$SwordHit/Sword.disabled = false
 			velocity.x = ATTACK_SPEED
 			$AnimatedSprite.flip_h = false
 			$AnimatedSprite.play("fighting")
-		elif Input.is_action_pressed("ui_left") and Input.is_action_pressed("ui_attack"):
+		elif Input.is_action_pressed("ui_left") and not in_ladder_area and Input.is_action_pressed("ui_attack"):
 			$SoundSword.play()
 			$SwordHit/Sword.disabled = false
 			velocity.x = -ATTACK_SPEED
 			$AnimatedSprite.flip_h = true
 			$AnimatedSprite.play("fighting")
-		elif Input.is_action_pressed("ui_attack"):
+		elif Input.is_action_pressed("ui_attack") and not in_ladder_area:
 			$SoundSword.play()
 			$SwordHit/Sword.disabled = false
 			velocity.x = 0
@@ -84,28 +89,15 @@ func _physics_process(_delta):
 			velocity.y = -CLIMB_VELOCITY
 			$AnimatedSprite.play("climbing")
 		else:
-			velocity.x = 0
-			$AnimatedSprite.play("idle")
+			if not is_throwing:
+				velocity.x = 0
+				$AnimatedSprite.play("idle")
 	
 	velocity.y = velocity.y + GRAVITY
 	
 	if Input.is_action_just_pressed("ui_jump") and is_on_floor() and not in_ladder_area:
 		velocity.y = JUMPFORCE
 		$SoundJump.play()
-		
-	if Input.is_action_just_pressed("ui_focus_next") and has_fireballs:
-		var fireball = FIREBALL.instance()
-		
-		if sign($Position2D.position.x) == 1:
-			fireball.set_fireball_dir(1)
-		else:
-			fireball.set_fireball_dir(-1)
-		
-		get_parent().add_child(fireball)
-		fireball.position = $Position2D.global_position
-		$SoundFireball.play()
-		
-		emit_signal("fireball_used")
 		
 	velocity = move_and_slide(velocity, Vector2.UP)
 	
@@ -165,6 +157,20 @@ func _on_DieTimer_timeout():
 	queue_free()
 	get_tree().change_scene("res://Scenes/GameOver.tscn")
 
+func _on_ThrowTimer_timeout():
+	var fireball = FIREBALL.instance()
+		
+	if sign($Position2D.position.x) == 1:
+		fireball.set_fireball_dir(1)
+	else:
+		fireball.set_fireball_dir(-1)
+	
+	get_parent().add_child(fireball)
+	fireball.position = $Position2D.global_position
+	$SoundFireball.play()
+	emit_signal("fireball_used")
+	is_throwing = false
+
 func _on_Ladder_body_entered(body):
 	if body.name == "Hero":
 		in_ladder_area = true
@@ -190,4 +196,4 @@ func _on_HUD_has_fireballs():
 	has_fireballs = true
 
 func _on_FallZone_body_entered(_body):
-	get_tree().change_scene("res://Scenes/GameOver.tscn")	
+	get_tree().change_scene("res://Scenes/GameOver.tscn")
