4c572a3c7d4db28c9f5aaba7d02e12711c567105
keithrdavis <keith.davis@zunisoft.com>
2023-03-23 13:52:59 -0700

    Fixed transition for pause menu

--- Scripts/MenuButton.gd
+++ Scripts/MenuButton.gd
@@ -4,6 +4,13 @@ func _ready():
 	process_mode = Node.PROCESS_MODE_ALWAYS
 
 func _on_MenuButton_pressed():
+	var hud = get_tree().get_root().find_child("HUD", true, false)
+	var pause = get_tree().get_root().find_child("PauseMenu", true, false)
+	
+	if pause and hud:
+		hud.visible = false
+		pause.visible = false
+	
 	if get_tree().paused:
 		get_tree().paused = false	
 	
