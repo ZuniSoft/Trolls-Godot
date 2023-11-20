715f26357108bfc12cb1854e5db3a550ebf28b39
keithrdavis <keith.davis@zunisoft.com>
2023-11-20 07:49:58 -0800

    Added file for movie maker mode

--- Scripts/Ogre2.gd
+++ Scripts/Ogre2.gd
@@ -20,7 +20,7 @@ func _on_DieTimer_timeout():
 	super._on_DieTimer_timeout()
 	
 	RoomState.reset_rooms()
-	GameState.set_next_level(3, false)
+	GameState.set_next_level(3, true)
 	
 	get_node("../../HUD").visible = false
 	
